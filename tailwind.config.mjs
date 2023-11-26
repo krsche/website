a9236bdd321e2e47f3a42f7c8a07d3c429ac82d3
Fabian Kirschner <fabian.kirschner@gmail.com>
2023-11-26 19:55:13 +0100

    fix: footer links & screen size order in tailwind cfg

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,8 +6,8 @@ export default {
   theme: {
     extend: {},
     screens: {
-      xs: "520px",
       xxs: "400px",
+      xs: "520px",
       sm: "640px",
       md: "768px",
       lg: "1024px",
