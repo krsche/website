98b9e0c49baf735fe54b49b183caa79143b3d1c4
Fabian Kirschner <fabian.kirschner@gmail.com>
2023-12-03 18:49:32 +0100

    feat: add blog (with example articles)

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,7 +11,19 @@ const projectsCollection = defineCollection({
   }),
 });
 
+const blogCollection = defineCollection({
+  type: "content", // 'content' for markdown, 'data' for json/yaml
+  schema: z.object({
+    title: z.string(),
+    timetoreadmins: z.number(),
+    date: z.string(),
+    tags: z.array(z.string()),
+    image: z.string(),
+  }),
+});
+
 // Below keys should match collections directory name in "src/content"
 export const collections = {
   projects: projectsCollection,
+  blog: blogCollection,
 };
