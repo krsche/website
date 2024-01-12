c5e47683dd52232514c59ac5a61db262b31a43c0
Fabian Kirschner <fabian.kirschner@gmail.com>
2024-01-12 17:35:57 +0100

    feat: add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [react(), tailwind()],
 });
